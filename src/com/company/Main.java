f58b447a97d3e1525d3e4adb50912a1db2a445d4
DashaStanovova <97908383+DashaStanovova@users.noreply.github.com>
2022-01-28 22:43:01 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,33 +1,29 @@
-input A
-input B
-input C
-if (A<B) AND (B<C) then
-        output A
-        output B
-        output C
-end if
-if (A<C) AND (C<B) then
-        output A
-        output C
-        output B
-end if
-if (B<A) AND (A<C) then
-        output B
-        output A
-        output C
-end if
-if (B<C) AND (C<A) then
-        output B
-        output C
-        output A
-end if
-if (C<A) AND (A<B) then
-        output C
-        output A
-        output B
-end if
-if (C<B) AND (B<A) then
-        output C
-        output B
-        output A
-end if
+output "Insert your credit card"
+output "Press 1 if card was entered the right side and Press 2 if it was entered the wrong side"
+input SIDE
+loop while SIDE==2
+    output "Insert the credit card again"
+    output "Press 1 if card was entered the right side and Press 2 if it was entered the wrong side"
+    input SIDE
+end loop
+output "Enter the pincode"
+output "Press 1 if pin-code was right and Press 2 if pin-code was wrong"
+input PINCODE
+loop while PINCODE==2
+    output "Enter the pin-code again"
+    output "Press 1 if pin-code was right and Press 2 if pin-code was wrong"
+    input PINCODE
+end loop
+output "Enter withdrawal sum"
+input BALANCE
+FLAG=true
+loop while FLAG
+    input SUM
+    if SUM>BALANCE then
+        output "Change your withdrawal sum"
+    end if
+    if SUM<BALANCE then
+        FLAG=false
+    end if
+end loop
+output "You have succesfully withdrawn " + SUM 
